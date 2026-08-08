abafcf767529b59a13ac7618b6c98655beb19e0a
bucketman09 <njsprouse20@gmail.com>
2026-08-08 00:00:44 -0400

    comines library and desk ideas :)

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 console.log("scripts loaded");
 
-const dragElements = document.querySelectorAll(".draggable"); 
+const dragElements = document.querySelectorAll(".draggable, .gravity"); 
+
 
 function onMouseDrag(event, element) {
-    console.log("click!")
     let leftValue = parseInt(window.getComputedStyle(element).left);
     let topValue = parseInt(window.getComputedStyle(element).top);
     element.style.left = `${leftValue + event.movementX}px`;
     element.style.top = `${topValue + event.movementY}px`;
-}
+    element.style.cursor = "grabbing";
+    }
 
 dragElements.forEach((element) => {
     element.addEventListener("mousedown", (e) => {
@@ -20,23 +21,3 @@ dragElements.forEach((element) => {
         }, { once: true });
     });
 });
-
-dragElements.forEach((element) => {
-    element.addEventListener("touchstart", (e) => {
-        const onMove = (event) => onMouseDrag(event, element); 
-
-        document.addEventListener("touchmove", onMove);
-        document.addEventListener("touchend", () => {
-            document.removeEventListener("touchmove", onMove);
-        }, { once: true });
-    });
-});
-
-
-function place_elems(){
-    var x = console.log(localStorage.getItem("x"))
-    var y = console.log(localStorage.getItem("y"))
-    document.getElementById("note").style.left = x;
-    document.getElementById("note").style.top = y;
-    
-}
